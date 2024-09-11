e8b7ebbde5e8ad9855b7f4b9b390f53ddbe2b138
Tox <67845762+ToxPlayers@users.noreply.github.com>
2024-09-11 23:26:52 +0300

    Update SingeltonMono.cs

--- Singeltons/SingeltonMono.cs
+++ Singeltons/SingeltonMono.cs
@@ -1,14 +1,8 @@
 using UnityEngine; 
-using System.Collections.Generic;
-using Unity.Collections;
 
-abstract public class SingletonMono<T> : SingletonBase where T : MonoBehaviour
+abstract public class SingletonMono<T> : MonoBehaviour  where T : MonoBehaviour
 {
     private static T _instance = null;
-    bool isAwakeCalled;
-    /// <summary>
-    /// Is there any instance of <typeparamref name="T"></typeparamref>
-    /// </summary>
     public static bool HasInstance => _instance; 
     public static T Instance
     { 
@@ -25,40 +19,38 @@ abstract public class SingletonMono<T> : SingletonBase where T : MonoBehaviour
             if (_instance != null)
             {
                 var instanceSing = _instance as SingletonMono<T>; 
-
                 instanceSing.OnInstanceSet();
-                if (!instanceSing.isAwakeCalled)
-                    instanceSing.Awake();
-
-                if (instanceSing.enabled)
-                    instanceSing.OnEnable();
-                else
-                    instanceSing.OnDisable();
             } 
         }
-        get => _instance ??= FindObjectOfType<T>();   
-    }   
-
-    static public T FindAndForceInstance()
-    {
-        if ( ! _instance )
+        get
         {
-            var inst = FindObjectOfType<T>();
-            if (inst)
-                (inst as SingletonMono<T>).ForceInstance();
-            return inst;
-        } 
-        return _instance;
+            if (!_instance)
+            {
+                FindAndForceInstanceAtRootGOs();
+                return _instance ??=  FindFirstObjectByType<T>(FindObjectsInactive.Include);
+            }
+            return _instance;
+        }
+    }  
+
+    static public T FindAndForceInstanceAtRootGOs()
+	{
+        if (_instance)
+            return _instance;
+
+        if ( UnityExtensions.TryGetRootComp<T>(out var rootT) ) 
+            return Instance = rootT; 
+
+        var inst = FindFirstObjectByType<T>();
+        if (inst)
+            (inst as SingletonMono<T>).ForceInstance();
+        return inst;
     }
-    /// <summary>
-    /// Is this the instance class for <typeparamref name="T"></typeparamref>?
-    /// </summary>
-    public override bool IsInstance { get => _instance && this == _instance; }
-    /// <summary>
-    /// Force this to be the instance class for <typeparamref name="T"></typeparamref>
-    /// </summary>
-    public override void ForceInstance() => Instance = this as T;
-    protected virtual void Awake()
+     
+    public bool IsInstance { get => _instance && this == _instance; } 
+    public void ForceInstance() => Instance = this as T;   
+
+	protected virtual void Awake()
 	{  
 #if UNITY_EDITOR
         var prefabStatus = UnityEditor.PrefabUtility.GetPrefabInstanceStatus(gameObject);
@@ -66,124 +58,30 @@ abstract public class SingletonMono<T> : SingletonBase where T : MonoBehaviour
 		{
 #endif
             T thisT = this as T;
-            if (HasInstance)
-            {
-                if (IsInstance)
-                    OnInstanceAwake();
-                else 
-                    OnNoneInstanceAwake();
-            }
-            else 
+            if (!HasInstance) 
                 Instance = thisT;  
 #if UNITY_EDITOR
         }
-#endif
-        isAwakeCalled = true;
-
-        OnAnyAwake(); 
+#endif 
     }
-
-
-	void OnEnable()
+	protected virtual void OnEnable()
     { 
-        T  thisT = this as T;
-
-        if (_instance != null && _instance != thisT) 
-            OnNoneInstanceEnabled(); 
-        else
-        {
+        T  thisT = this as T; 
+        if(!HasInstance)
             Instance = thisT; 
-            OnInstanceEnabled(); 
-        }  
     }
-    public void OnDestroy()
+    protected virtual void OnDestroy()
 	{
-
 		if (_instance == this)
-        {
-            OnInstanceRemoved();
-            OnInstanceDestroy();
             Instance = null;
-        }
+	}
 
-		OnAnyDestroy();
-
-	} 
-    void OnDisable()
-	{
-        if (IsInstance)
-            OnInstanceDisabled();
-	} 
     /// <summary>
-    /// Called on Instance or None instance Awake
+    /// Called when this object is set to Instance of <typeparamref name="T"></typeparamref>
     /// </summary>
-    protected virtual void OnAnyAwake() { }  
-    /// <summary>
-    /// Called when Instance or None instance destroy
-    /// </summary>
-    protected virtual void OnAnyDestroy() { }
-    /// <summary>
-    /// alled when this object is set to Instance of <typeparamref name="T"></typeparamref>
-    /// </summary>
-	protected virtual void OnInstanceSet() {}
-    /// <summary>
-    /// Called when this Instance object is disabled
-    /// </summary>
-    protected virtual void OnInstanceDisabled() { }
+    protected virtual void OnInstanceSet() {}
     /// <summary>
     /// Called when this object is no longer the Instance object.
     /// </summary>
-    protected virtual void OnInstanceRemoved() { }
-    /// <summary>
-    /// Called when this instance object is destroyed
-    /// </summary>
-    protected virtual void OnInstanceDestroy() { }
-    /// <summary>
-    /// Called when this instance object is enabled
-    /// </summary>
-    protected virtual void OnInstanceEnabled() { }
-    /// <summary>
-    /// Called when none instance object is enabled
-    /// </summary>
-    protected virtual void OnNoneInstanceEnabled() { }
-    /// <summary>
-    /// Called on instance object awake, or when instance was set and instance awake wasn't called.
-    /// May be called in the editor when <see cref="ForceInstance"/> is called. 
-    /// </summary>
-    protected virtual void OnInstanceAwake() { }
-    /// <summary>
-    /// Called on none instance awake
-    /// </summary>
-    protected virtual void OnNoneInstanceAwake() { }
-
+    protected virtual void OnInstanceRemoved() {} 
 }
-
-
-
-/// <summary>
-/// This class is mostly for the custom editor implementation.
-/// </summary>
-public abstract class SingletonBase : MonoBehaviour
-{
-    public abstract bool IsInstance { get; }
-    public abstract void ForceInstance();
-}
-
-#if UNITY_EDITOR
-[UnityEditor.CustomEditor(typeof(SingletonMono<>), true)]
-public class SingletonMonoEditor : UnityEditor.Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        var targetMono = target as SingletonBase;
-        if( ! ReferenceEquals(targetMono ,null) )
-        {
-            var isInstance = targetMono.IsInstance;
-            GUI.enabled = !isInstance;
-            if( GUILayout.Button(isInstance ? "Instance Class" : "Force Instance Class") )
-                targetMono.ForceInstance();
-        }
-    } 
-}
-#endif
